09f16ddfdd41e74881061b7a5aacb7ba102b5753
Jonathan A. Saddler <1129384+jazad136@users.noreply.github.com>
2021-08-10 14:12:09 -0400

    [DBASE] For #2 Create annotations in StaffMember.java file

--- room-web-app/src/main/java/com/jsaddlercs/lil/sbet/landon/roomwebapp/models/StaffMember.java
+++ room-web-app/src/main/java/com/jsaddlercs/lil/sbet/landon/roomwebapp/models/StaffMember.java
@@ -1,9 +1,30 @@
 package com.jsaddlercs.lil.sbet.landon.roomwebapp.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="EMPLOYEE")
 public class StaffMember {
+	@Id
+	@Column(name="EMPLOYEE_ID")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
     private String employeeId;
+    
+	@Column(name="FIRST_NAME") 
     private String firstName;
+    
+	@Column(name="LAST_NAME") 
     private String lastName;
+    
+	@Column(name="POSITION") 
+	@Enumerated(EnumType.STRING)
     private Position position;
     
     public StaffMember() { }
