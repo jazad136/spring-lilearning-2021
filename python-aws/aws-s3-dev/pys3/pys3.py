8dacfbe6dc61e72177caa772c1bb3dfd522d0f3a
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-09-15 21:54:11 -0400

    Sep15 - AWS Python Project Initial Commit

---
+++ python-aws/aws-s3-dev/pys3/pys3.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""A python script for working with amazon S3"""
+
+def main():
+    """entry point"""
+    print('hello world')
+    
+if __name__ == '__main__':
+    main()
