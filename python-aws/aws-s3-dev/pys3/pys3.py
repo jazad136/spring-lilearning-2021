c19b98be53cba3813f5f21a556c6a9cbb199db05
Jonathan A. Saddler <1129384+jazad136@users.noreply.github.com>
2021-09-25 10:58:08 -0400

    Python S3: adding createBucket() method to AWS Python

--- python-aws/aws-s3-dev/pys3/pys3.py
+++ python-aws/aws-s3-dev/pys3/pys3.py
@@ -4,12 +4,14 @@ import os
 from botocore.exceptions import ClientError
 ACCESS_KEY = 'AWS_ACCESS_KEY_ID'
 SECRET_KEY = 'AWS_SECRET_ACCESS_KEY'
+REGION = 'us-east-2'
 PRI_BUCKET_NAME = 'jaslil'
 TRANSIENT_BUCKET_NAME = 'jaslil2'
 f1 = 'lil1.txt'
 f2 = 'lil2.txt'
 f3 = 'lil3.txt'
-base = os.path.join('C:', 'Users', "jsaddle", "code", "repos", "plain", "AWS_Moley_exercise2")
+
+base = os.path.join(os.path.sep,"C:",os.path.sep, "Users", "jsaddle", "code", "repos", "plain", "AWS_Moley_exercise2")
 DIR = os.path.join(base, 'aws-s3-dev', 'pys3')
 DOWN_DIR = os.path.join(base, 'aws-s3-dev', 's3alt')
 #DIR = 'C:/Users/jsaddle/code/repos/plain/AWS_Moley_exercise2/aws-s3-dev/pys3'
@@ -18,14 +20,20 @@ DOWN_DIR = os.path.join(base, 'aws-s3-dev', 's3alt')
 def main():
     '''Entry point'''
     import boto3
-    access = os.getenv(ACCESS_KEY)
+    # might not be a good idea to upload these to git... 
+    # storing these elsewhere now...
+    access = os.getenv(ACCESS_KEY) 
     secret = os.getenv(SECRET_KEY)
-    s3 = boto3.resource('s3', aws_access_key_id=access, aws_secret_acccess_key=secret)
+    s3 = boto3.resource('s3', 
+                        aws_access_key_id=access,
+                        aws_secret_access_key=secret)
+    
     create_bucket(TRANSIENT_BUCKET_NAME, s3)
     
 def create_bucket(name, s3):
     try:
-        bucket = s3.create_bucket(bucket=name)
+        location = {'LocationConstraint': REGION}
+        bucket = s3.create_bucket(Bucket=name, CreateBucketConfiguration=location)
     except ClientError as ce:
         print('error', ce)
         
