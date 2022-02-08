24898566b68ca20bbbf0b342a55782beb6b60669
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-02-07 21:03:48 -0500

    Launch ExploreCali Docker Container

---
+++ explorecali-docker/src/test/java/com/example/ec/domain/RegionConverterTest.java
@@ -0,0 +1,24 @@
+package com.example.ec.domain;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by Mary Ellen Bowman.
+ */
+public class RegionConverterTest {
+    private RegionConverter converter = new RegionConverter();
+    @Test
+    public void convertToDatabaseColumn() throws Exception {
+        assertThat(converter.convertToDatabaseColumn(Region.Central_Coast), is(Region.Central_Coast.getLabel()));
+    }
+
+    @Test
+    public void convertToEntityAttribute() throws Exception {
+        assertThat(converter.convertToEntityAttribute(Region.Central_Coast.getLabel()), is(Region.Central_Coast));
+    }
+
+}
